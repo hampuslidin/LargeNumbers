0ba11aeb67af4d8fa7aad61bef37b7ff074e2884
Hampus L. (OSX) <hampuslidin@gmail.com>
2015-05-25 15:07:24 +0200

    Initial Commit

---
+++ LargeNumbers/LargeNumbers.h
@@ -0,0 +1,19 @@
+//
+//  LargeNumbers.h
+//  LargeNumbers
+//
+//  Created by Hampus Lidin on 2015-05-25.
+//  Copyright (c) 2015 Lidin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for LargeNumbers.
+FOUNDATION_EXPORT double LargeNumbersVersionNumber;
+
+//! Project version string for LargeNumbers.
+FOUNDATION_EXPORT const unsigned char LargeNumbersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LargeNumbers/PublicHeader.h>
+
+
